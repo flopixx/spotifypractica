b6b67c1a1247b5901465162e628d759a1297cd98
flopixx <flor.vel96@gmail.com>
2020-12-18 12:48:29 -0300

    proyecto practica

---
+++ jquery.js
@@ -0,0 +1,12 @@
+
+let uno=document.getElementById("mostrar-menu")
+let menu=document.querySelector(".mostrar-menu-principal")
+
+
+
+uno.addEventListener('click',function(){
+menu.classList.toggle("mostrar-menu-principal");
+
+
+});
+
